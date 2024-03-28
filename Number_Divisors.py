1d6ab1a4e2cf5527e7ff53bf10566ec4f2accc6f
Thna17 <Hangbrathna10@gmail.com>
2024-03-28 11:07:03 +0700

    1. Introduction to Problem Solving: [4] Number of divisors

--- Number_Divisors.py
+++ Number_Divisors.py
@@ -1,8 +1,12 @@
-def solve(n10):    
+import time
+def solve(n):    
     count = 0
-    for i in range(1, N + 1):
+    for i in range(1, n + 1):
         if N % i == 0:
             count +=1
     return count
 N = int(input())
+start = time.time()
 print(solve(N))
+end = time.time()
+print(f'slove() elapsed time: {end - start}')
