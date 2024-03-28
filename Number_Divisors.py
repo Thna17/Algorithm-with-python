4bda9c9ffcafa95fb5f4b8adcfe2282d29b6626a
Thna17 <Hangbrathna10@gmail.com>
2024-03-28 11:44:47 +0700

    1. Introduction to Problem Solving: [4] Number of divisors

--- Number_Divisors.py
+++ Number_Divisors.py
@@ -1,12 +1,18 @@
 import time
-def solve(n):    
+import math
+
+def solve(n):
     count = 0
-    for i in range(1, n + 1):
-        if N % i == 0:
-            count +=1
+    sqrt_n = int(math.sqrt(n))
+    for i in range(1, sqrt_n + 1):
+        if n % i == 0:
+            count += 2
+    if sqrt_n * sqrt_n == n:
+        count -= 1
     return count
+
 N = int(input())
 start = time.time()
 print(solve(N))
 end = time.time()
-print(f'slove() elapsed time: {end - start}')
+print(f'solve() elapsed time: {end - start}')
